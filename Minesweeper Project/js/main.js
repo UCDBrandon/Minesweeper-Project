79001fa852ed896d22edcaa3fd562f900e50f394
Brandon <Brandon@DESKTOP-2DN6RJ3>
2018-12-19 15:08:32 -0800

    Added unopened tile image

--- Minesweeper Project/js/main.js
+++ Minesweeper Project/js/main.js
@@ -1,1 +1,28 @@
-﻿// Your code here!
+﻿(function () {
+
+    var game = {
+        'row': 16,
+        'col': 31
+    }
+
+    var tile = {
+        'img': null,
+        'x': 0,
+        'y': 0
+    }
+
+    var content = document.getElementById("mainContent");
+    for (var i = 0; i < game.row; i++) {
+        for (var j = 0; j < game.col; j++) {
+            var emptyTile = document.createElement("div");
+            var tileImage = document.createElement("img");
+
+            emptyTile.classList.add("tile");
+            tileImage.src = "images/unopened.png";
+            emptyTile.appendChild(tileImage);
+            content.appendChild(emptyTile);
+        }
+        var lineBreak = document.createElement("br");
+        content.appendChild(lineBreak);
+    }
+})();
