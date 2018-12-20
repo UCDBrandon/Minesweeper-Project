f322ff4dfc92353d2f0d84b3a17bae5ce2d142f9
Brandon <Brandon@DESKTOP-2DN6RJ3>
2018-12-19 19:49:30 -0800

    Redid restarting the game by replacing images.

--- Minesweeper Project/js/main.js
+++ Minesweeper Project/js/main.js
@@ -1,13 +1,16 @@
 ﻿var MinesweeperGame = function (row, col) {
     this.row = row,
     this.col = col,
+    this.numBombs = 99,
+    this.bombs = [],
+    this.boardGenerated = false,
     this.difficulty = "Expert"
 }
 
-MinesweeperGame.prototype.newGame = function () {
-    //Setup a new board by adding in elements to the main div
-    var content = document.getElementById("mainContent");
+MinesweeperGame.prototype.generateGame = function () {
+    //Setup the board and 2D array used to hold bombs
 
+    var content = document.getElementById("mainContent");
     // Row X Column
     for (var i = 0; i < this.row; i++) {
         for (var j = 0; j < this.col; j++) {
@@ -21,33 +24,6 @@ MinesweeperGame.prototype.newGame = function () {
         }
     }
 
-    this.assignBombs();
-}
-
-MinesweeperGame.prototype.clearBoard = function () {
-    //Clear the board followed by creating a new board.
-    //Can replace later to just change the image of each position instead of deleting
-    var content = document.getElementById("mainContent");
-    while (content.childElementCount != 0) {
-        content.removeChild(content.firstChild);
-    }
-
-    for (var i = 0; i < this.row; i++) {
-        for (var j = 0; j < this.col; j++) {
-            this.bombs[i][j] = 0;
-        }
-    }
-    //Start new game
-    this.newGame();
-}
-
-MinesweeperGame.prototype.assignBombs = function () {
-    var bombs = 99;
-    this.bombs = [];
-    var xCoord;
-    var yCoord;
-
-    //Create the 2d array similar to grid
     for (var i = 0; i < this.row; i++) {
         this.bombs.push([]);
         for (var j = 0; j < this.col; j++) {
@@ -55,9 +31,38 @@ MinesweeperGame.prototype.assignBombs = function () {
             this.bombs[i][j] = 0;
         }
     }
+    this.boardGenerated = true;
+}
+
+MinesweeperGame.prototype.newGame = function () {
+    if (this.boardGenerated === false) {
+        this.generateGame();
+    }
+    this.clearBoard();
+    this.assignBombs();
+}
+
+MinesweeperGame.prototype.clearBoard = function () {
+    //Clear the board followed by simply replacing the images.
+    var content = document.getElementById("mainContent");
+    for (var i = 0; i < this.row * this.col; i++) {
+        content.children[i].src = "images/unopened.png";
+    }
+
+    //Reset the bombs
+    for (var i = 0; i < this.row; i++) {
+        for (var j = 0; j < this.col; j++) {
+            this.bombs[i][j] = 0;
+        }
+    }
+}
+
+MinesweeperGame.prototype.assignBombs = function () {
+    var xCoord;
+    var yCoord;
 
     //Assign random bomb coordinates
-    for (var i = 0; i < bombs; i++) {
+    for (var i = 0; i < this.numBombs; i++) {
         xCoord = Math.floor(Math.random() * this.row);
         yCoord = Math.floor(Math.random() * this.col);
 
@@ -70,12 +75,15 @@ MinesweeperGame.prototype.assignBombs = function () {
     }
 
     //Debug
+    /*
     for (var i = 0; i < this.row; i++) {
         for (var j = 0; j < this.col; j++) {
             console.log("[" + i + "][" + j + "] " + this.bombs[i][j]);
         }
-    }
+    }*/
 }
+
 
 var game = new MinesweeperGame(16, 31);
 game.newGame();
+
